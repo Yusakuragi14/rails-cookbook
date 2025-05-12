cf67dc56291dad73b81517d94e1ef4d391a1903a
yu wong <sakuragi.yu@gmail.com>
2025-05-12 12:47:07 +0100

    STEP 8 - Routing, Controller and Views for Bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
   resources :categories, only: [:index, :show, :new, :create]
+   resources :bookmarks, only: [:new, :create] do
+   end
+  resources :bookmarks, only: [:destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
